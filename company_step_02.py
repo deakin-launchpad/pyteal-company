6b294672157168d56095b04bb204deb26f7099aa
Hemerooooo <hanmiaoxuan@gmail.com>
2023-01-18 15:35:02 +1100

    step_01 updated, step_02 is in developing

--- company_step_02.py
+++ company_step_02.py
@@ -18,8 +18,9 @@ def approval():
     # operation
     op_mint_coins = Bytes("mint_coins")
     op_mint_shares = Bytes("mint_Shares")
+    op_send_coins = Bytes("send_coins")
 
-    # initial company
+    # initialize company
     @Subroutine(TealType.none)
     def on_create():
         return Seq(
@@ -32,10 +33,10 @@ def approval():
 
     # create assets (coins or shares)
     @Subroutine(TealType.none)
-    def create_tokens(asset_name, asset_unit_name, asset_total, asset_decimal, asset_default_frozen, asset_reserve):
+    def create_tokens(asset_name, asset_unit_name, asset_total, asset_decimal, asset_default_frozen, asset_reserve_status):
         return Seq(
             InnerTxnBuilder.Begin(),
-            If(asset_reserve == Int(1))
+            If(asset_reserve_status == Int(1))
             .Then(
                 # create an asset that needs a reserve account
                 InnerTxnBuilder.SetFields(
@@ -85,7 +86,8 @@ def approval():
                 )
             ),
             # mint coins
-            create_tokens(Txn.application_args[1],Txn.application_args[2],Btoi(Txn.application_args[3]),Btoi(Txn.application_args[4]),Btoi(Txn.application_args[5]),Int(1)),
+            create_tokens(Txn.application_args[1], Txn.application_args[2], Btoi(
+                Txn.application_args[3]), Btoi(Txn.application_args[4]), Btoi(Txn.application_args[5]), Int(1)),
             # coins id
             App.globalPut(coins_key, (InnerTxn.created_asset_id())),
             # coins minted
@@ -112,11 +114,66 @@ def approval():
                 )
             ),
             # mint shares
-            create_tokens(Txn.application_args[1],Txn.application_args[2],Btoi(Txn.application_args[3]),Btoi(Txn.application_args[4]),Btoi(Txn.application_args[5]), Int(0)),
+            create_tokens(Txn.application_args[1], Txn.application_args[2], Btoi(
+                Txn.application_args[3]), Btoi(Txn.application_args[4]), Btoi(Txn.application_args[5]), Int(0)),
             # shares id
             App.globalPut(shares_key, InnerTxn.created_asset_id()),
             # shares minted
             App.globalPut(shared_indicator_key, Int(1)),
+            Approve(),
+        )
+
+    # send assets
+    @Subroutine(TealType.none)
+    def send_tokens(asset_id, amount):
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            # create an asset that needs a reserve account
+            InnerTxnBuilder.SetFields(
+                {
+                    TxnField.type_enum: TxnType.AssetTransfer,
+                    TxnField.xfer_asset: asset_id,
+                    TxnField.asset_receiver: Txn.accounts[1],
+                    TxnField.asset_amount: amount,
+                }
+            ),
+            InnerTxnBuilder.Submit(),
+        )
+
+    # check assets holding information
+    @Subroutine(TealType.anytype)
+    def check_assets_amount(asset_id):
+        return Return(
+            AssetHolding.balance(
+                Txn.sender(), Txn.assets[0]).value()
+            )
+
+    # Send to reserve account
+    @Subroutine(TealType.none)
+    def send_coins():
+        # to store coins id
+        coins_id = ScratchVar(TealType.uint64)
+        return Seq(
+            # basic sanity checks
+            program.check_self(
+                group_size=Int(1),
+                group_index=Int(0),
+            ),
+            program.check_rekey_zero(1),
+            # get asset ID of coins
+            coins_id.store(App.globalGet(coins_key)),
+            Assert(
+                And(
+                    # make sure the company has created coins
+                    App.globalGet(minted_indicator_key) == Int(1),
+                    # make sure the company own enough coins
+                    check_assets_amount(
+                        coins_id.load()) >= Btoi(Txn.application_args[1]),
+                    # operation, amount of coins
+                    Txn.application_args.length() == Int(2),
+                )
+            ),
+            send_tokens(coins_id.load(), Txn.application_args[1]),
             Approve(),
         )
 
@@ -135,6 +192,10 @@ def approval():
                     Txn.application_args[0] == op_mint_shares,
                     mint_shares(),
                 ],
+                [
+                    Txn.application_args[0] == op_send_coins,
+                    send_coins(),
+                ],
             ),
             Reject(),
         ),
@@ -145,10 +206,10 @@ def clear():
     return Approve()
 
 
-with open('company_step_01.teal', 'w') as f:
+with open('company_step_02.teal', 'w') as f:
     compiled = compileTeal(approval(), Mode.Application, version=5)
     f.write(compiled)
 
-with open("company_step_01_clear.teal", "w") as f:
+with open("company_step_02_clear.teal", "w") as f:
     compiled = compileTeal(clear(), Mode.Application, version=5)
     f.write(compiled)
